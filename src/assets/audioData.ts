ff8d7cf1e8d34401b631e928d6d38a91e16d4307
Fernando <fpinheiro@nwsoft.com.br>
2023-01-11 16:51:03 -0300

    fix: Paths & title

--- src/assets/audioData.ts
+++ src/assets/audioData.ts
@@ -1,31 +1,37 @@
 export const audios = [
   {
     name: "Emotional Mess",
-    music: "src/assets/audios/Emotional Mess - Amy Lynn & the Honey Men.mp3",
+    music:
+      "https://raw.githubusercontent.com/fernandoigor/practices-bora-codar-music-player-react/main/src/assets/audios/Emotional Mess - Amy Lynn & the Honey Men.mp3",
     artist: "Amy Lynn & the Honey Men",
     image:
-      "src/assets/audios/images/Emotional Mess - Amy Lynn & the Honey Men.jpeg",
+      "https://raw.githubusercontent.com/fernandoigor/practices-bora-codar-music-player-react/main/src/assets/audios/images/Emotional Mess - Amy Lynn & the Honey Men.jpeg",
   },
 
   {
     name: "God Rest Ye Merry Gentlmen",
-    music: "src/assets/audios/God Rest Ye Merry Gentlmen - DJ Williams.mp3",
+    music:
+      "https://raw.githubusercontent.com/fernandoigor/practices-bora-codar-music-player-react/main/src/assets/audios/God Rest Ye Merry Gentlmen - DJ Williams.mp3",
     artist: "DJ Williams",
     image:
-      "src/assets/audios/images/God Rest Ye Merry Gentlmen - DJ Williams.jpeg",
+      "https://raw.githubusercontent.com/fernandoigor/practices-bora-codar-music-player-react/main/src/assets/audios/images/God Rest Ye Merry Gentlmen - DJ Williams.jpeg",
   },
 
   {
     name: "Ice & Fire",
-    music: "src/assets/audios/Ice & Fire - King Canyon.mp3",
+    music:
+      "https://raw.githubusercontent.com/fernandoigor/practices-bora-codar-music-player-react/main/src/assets/audios/Ice & Fire - King Canyon.mp3",
     artist: "King Canyon",
-    image: "src/assets/audios/images/Ice & Fire - King Canyon.jpeg",
+    image:
+      "https://raw.githubusercontent.com/fernandoigor/practices-bora-codar-music-player-react/main/src/assets/audios/images/Ice & Fire - King Canyon.jpeg",
   },
 
   {
     name: "Oh Christmas Tree",
-    music: "src/assets/audios/Oh Christmas Tree - DJ Williams.mp3",
+    music:
+      "https://raw.githubusercontent.com/fernandoigor/practices-bora-codar-music-player-react/main/src/assets/audios/Oh Christmas Tree - DJ Williams.mp3",
     artist: "DJ Williams",
-    image: "src/assets/audios/images/Oh Christmas Tree - DJ Williams.jpeg",
+    image:
+      "https://raw.githubusercontent.com/fernandoigor/practices-bora-codar-music-player-react/main/src/assets/audios/images/Oh Christmas Tree - DJ Williams.jpeg",
   },
 ];
